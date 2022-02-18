9124ac4176d486ba7316f45841b9b36c55d838cf
Pranay Ashok Ramteke <69661033+Phoenix600@users.noreply.github.com>
2022-02-18 02:31:21 -0800

    01_Linked_list.cpp
    Basic Program on Linked list...

--- 01_liked_list.cpp
+++ 01_liked_list.cpp
@@ -1,6 +1,47 @@
 #include<iostream>
 
+class Node{
+    public:
+    int key;
+    Node* next;
+};
+
 int main()
 {
-  return 0;
+    Node* head;
+    Node* first = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+    Node* fourth = NULL;
+    
+    // allocating memory for the three nodes 
+    first = new Node;
+    second = new Node;
+    third = new Node;
+    fourth = new Node;
+    
+    // Setting data to the node
+    first->key = 12;
+    second->key = 13;
+    third->key = 14;
+    fourth->key = 15;
+    
+    // Connecting the nodes
+    first->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = NULL;
+    
+    // printing the nodes 
+    head = first;
+    
+     std::cout<<"Keys in the linked list....\n";
+     
+    while(head!=NULL)
+    {
+        std::cout<<head->key<<" ";
+        head = head->next;
+    }
+    
+    return 0;
 }
